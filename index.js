519a81358de625ca886b48bc3cd8b019971a8155
Pedro Fialho <pedrofialho000@gmail.com>
2025-09-18 10:22:53 -0300

    Create front-end

--- index.js
+++ index.js
@@ -6,37 +6,50 @@ const UID = require('./model/uid')
 const port = 3000
 const db = new UidDb()
 
-app.get("/", (req, res)=> res.send('RFID Scanner'))
+app.use(express.json())
 
-app.get("/consultar", (req, res) => {
-    db.consultar("123", (err, resultado) => {
+app.use('/css', express.static(path.join(__dirname, 'css')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'index.html'))
+})
+
+app.get('/cadastrar', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'cadastrar.html'));
+});
+
+app.get('/consultar', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'consultar.html'));
+})
+
+app.get("/consultar-uid", (req, res) => {
+    const uid = req.query.uid
+    db.consultar(uid, (err, resultado) => {
         if(err) {
             console.log("Erro:", err);
             res.status(500).send("Erro no servidor");
         } else if(resultado) {
             console.log("UID encontrado:", resultado);
-            res.send(`UID encontrado: ${JSON.stringify(resultado)}`);
+            res.json({ status: "sucesso", resultado });
         } else {
             console.log("UID não encontrado");
             res.redirect('/cadastrar')
         }
-    });
-});
+    })
+})
 
-app.get("/cadastrar", (req, res) => {
-    const objUid = new UID('123', 'Pedro');
+app.post("/cadastrar", (req, res) => {
+    const { uid, nome } = req.body;  
+    const objUid = new UID(uid, nome);
 
     db.cadastrar(objUid, (err) => {
         if(err) {
             console.log("Erro:", err);
-            res.status(500).send("Erro no servidor");
-        } else {
-            console.log("UID cadastrado:", objUid);
-            res.send(`UID cadastrado: ${JSON.stringify(objUid)}`);
-        }
+            return res.status(500).send("Erro no servidor");
+        } 
+        console.log("UID cadastrado:", objUid);
+        res.send(`UID cadastrado: ${JSON.stringify(objUid)}`);
     });
 });
-
-
 
 app.listen(port, () => console.log(`Aplicação aberta na porta ${port}`))
